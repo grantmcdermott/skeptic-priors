f6173957c6e9b3ec9d0c317dfc4cd5620c62c334
Grant McDermott <grant.mcdermott@gmail.com>
2016-11-08 15:11:11 -0800

    Converted recursive regressions to nested lapply functions

--- Recursive/jags-recursive.R
+++ Recursive/jags-recursive.R
@@ -1,86 +1,41 @@
-a <- a + 1
-
-if(recurse_type == "historic"){
-  clim_df <- 
-    climate %>%
-    filter(year >=  n)
-}
-
-if(recurse_type == "future"){
-  clim_df <- 
-    climate %>%
-    filter(year <=  n)
-}
-
-yr_min <- min(clim_df$year)
-
 ##------------------------------------------------------------------------------
-## THE BUGS/JAGS MODEL.
-## Note: Removing y_pred b/c predictions into the future not needed for recursive regs
+## THE BUGS/JAGS MODEL. 
+## Notes: Using "_sim" versions in case of future recursive type. Also dropping 
+## y_pred variable b/c don't need predictions for recursive estimates.
 
 N <- nrow(clim_df)
 
 mod_string <- paste(
   "model{
-  
-  for(t in 1:N) {
-  mu[t] <- alpha + beta*trf[t] + gamma*volc_sim[t] + delta*soi_sim[t] + eta*amo_sim[t]
-  had_sim[t]  ~ dnorm(mu[t], tau)
-  # y_pred[t] ~ dnorm(mu[t], tau) ## For predictions into the future
-  }
-  ",
-  
-  if (prior_type == "ni") {
-  "
-  ## Noninformative prior on beta:         
-  mu_beta <- 0
-  sigma_beta <- 100
-  "
-  }else{
-  "
-  ## Prior on beta: One of four subjective prior, conviction combinations...
     
-  # 1) Mod. lukewarmer: TCR ~ N(1, 0.25^2). Mean of 1 °C and uncertainty range of 1.0 °C (95% probability).
-  #    Converting to beta (= TCR/3.71): B ~ N( 1/3.71, (0.25/3.71)^2 )
-  # 2) Strong lukewarmer: TCR ~ N(1, 0.065^2). Mean of 1 °C and uncertainty range of 0.25 °C (95% probability).
-  #    Converting to beta (= TCR/3.71): B ~ N( 1/3.71, (0.065/3.71)^2 )
-  # 3) Mod. denier: TCR ~ N(0, 0.25^2). Mean of 0 °C and uncertainty range of 1.0 °C (95% probability).
-  #    Converting to beta (= TCR/3.71): B ~ N( 0/3.71, (0.25/3.71)^2 )
-  # 4) Strong denier: TCR ~ N(0, 0.065^2). Mean of 0 °C and uncertainty range of 0.25 °C (95% probability).
-  #    Converting to beta (= TCR/3.71): B ~ N( 0/3.71, (0.065/3.71)^2 )
+    for(t in 1:N) {
+      mu[t] <- alpha + beta*trf[t] + gamma*volc_sim[t] + delta*soi_sim[t] + eta*amo_sim[t]
+      had_sim[t]  ~ dnorm(mu[t], tau)
+    }
+    ", 
+  paste0(
+    "
+            mu_beta <- ", mu_beta),
+  paste0(
+    "
+            sigma_beta <- ", sigma_beta),
   
-  "},
-  
-  if (prior_type == "luke") {
-  "mu_beta <- 1/3.71
-  "},
-  if (prior_type == "den") {
-  "mu_beta <- 0
-  "},
-  if (convic_type == "mod") {
-  "sigma_beta <- 0.25/3.71
-  "},
-  if (convic_type == "strong") {
-  "sigma_beta <- 0.065/3.71
-  "}, 
-  
-  "
-  ## Priors for all parameters   
-  alpha ~ dnorm(0, 0.0001)            ## intercept
-  beta ~ dnorm(mu_beta, tau_beta)     ## trf coef
-  tau_beta <- pow(sigma_beta, -2)
-  gamma ~ dnorm(0, 0.0001)            ## volc coef
-  delta ~ dnorm(0, 0.0001)            ## soi coef
-  eta ~ dnorm(0, 0.0001)              ## amo coef
-  sigma ~ dunif(0, 100)               ## Residual std dev
-  tau <- pow(sigma, -2)  	          
-  had0 ~ dnorm(0.0, 1.0E-6)           ## Initialising value
-  }" 
-    ) 
+    "
 
-bugs_file <- paste0("./Recursive/BUGSFiles/", prior_type, "-", 
-                    convic_type, ".txt")
-if(prior_type == "ni"){bugs_file <- gsub("--", "-", bugs_file)}
+    ## Priors for all parameters   
+    alpha ~ dnorm(0, 0.0001)            ## intercept
+    beta ~ dnorm(mu_beta, tau_beta)     ## trf coef
+      tau_beta <- pow(sigma_beta, -2)
+    gamma ~ dnorm(0, 0.0001)            ## volc coef
+    delta ~ dnorm(0, 0.0001)            ## soi coef
+    eta ~ dnorm(0, 0.0001)              ## amo coef
+    sigma ~ dunif(0, 100)               ## Residual std dev
+      tau <- pow(sigma, -2)  	          
+    had0 ~ dnorm(0.0, 1.0E-6)           ## Initialising value
+    }" 
+) 
+bugs_file <- paste0("Recursive/BUGSFiles/", prior_type, "-", convic_type, recurse_type, ".txt")
+if(prior_type == "ni"){bugs_file <- gsub("--","-",bugs_file)}
 writeLines(mod_string, con = bugs_file)
 
 load.module("lecuyer") ## JAGS module uses lecuyer random number generator (to avoid overlap/correlation in a parallel format)
@@ -94,6 +49,7 @@ parLoadModule(cl, "lecuyer", quiet = T)
 data_list <- list("N" = N, "had_sim" = clim_df$had_sim, "trf" = clim_df$trf, 
                   "volc_sim" = clim_df$volc_sim, 
                   "soi_sim" = clim_df$soi_sim, "amo_sim" = clim_df$amo_sim)
+
 inits_list <- function() {
   list(alpha = 0, beta = 0, gamma = 0, delta = 0, eta = 0, sigma = 0.1)
 }
@@ -101,103 +57,38 @@ inits_list <- function() {
 ##------------------------------------------------------------------------------
 ## RUN THE CHAINS.
 
-parameters <- c("alpha", "beta", "gamma", "delta", "eta", "sigma"#, "y_pred"
-                )
+parameters <- c("alpha", "beta", "gamma", "delta", "eta", "sigma")
 par_inits <- parallel.inits(inits_list, n.chains = n_chains) # Initialisation
 
 parJagsModel(cl, name = "jags_mod", file = bugs_file, 
              data = data_list, inits = par_inits, n.chains = n_chains, n.adapt = 1000)
 parUpdate(cl, "jags_mod", n.iter = 1000) # burn-in
-mod_iters <- chain_length
+mod_iters <- chain_length/n_chains
 mod_samples <- parCodaSamples(cl, "jags_mod", variable.names = parameters, 
-                              n.iter = mod_iters, n.chain = n_chains) 
+                              n.iter = mod_iters, n.chain = n_chains)
 stopCluster(cl)
 
 ##------------------------------------------------------------------------------
 ## Get TCR coefficient (i.e. beta) and then use to get TCR.
 
-tcr <- as.matrix(mod_samples[, "beta"], iters = F) * rf2x
+## Convert coefficients MCMC list into data frame for later. First combines
+## all chains into one matrix.
+coefs_df <-
+  as.matrix(mod_samples[, c(1:6)], iters = F) %>%
+  data.frame() %>% 
+  tbl_df() %>%
+  gather(coef, values)
 
-rm(bugs_file, cl, data_list, mod_samples, mod_string, par_inits, parameters)
+## Posterior TCRs, temp prediction at 2100 (and coefficient values) ##  
+tcr <- 
+  data_frame(
+    beta = filter(coefs_df, coef == "beta")$values,
+    prior = paste0(prior_type, convic_type)
+    ) %>%
+  mutate(year_to = ifelse(recurse_type == "historic", yr_min, max(clim_df$year)))
 
+tcr$tcr <- tcr$beta * rf2x
 
-## Set figure if haven't done so by calling the proportional noninformative prior previously
-if(prior_type == "ni"){
-  
-  A <- A + 1 # For animation figures
-  pdf(file = paste0("./Recursive/TablesFigures/Animation/", 
-                    recurse_type,
-                    "/rec-tcr-", 1000 + A,".pdf"), 
-      width = 16, height = 10, family = "Palatino")
-  par(las = 1, 
-      mar = c(5.5, 5.7, 4, 2) + 0.1, 
-      mgp = c(4, 1.5, 0)) # Change margin and tick labels to give space for different font
-  plot(1, type = "n", 
-       ylab = "Density", xlab = expression(~degree~C), 
-       main = "", cex.lab = 2.5, cex.axis = 2.25, cex.main = 2.25, cex.sub = 2.25,
-       xlim = c(-1, 3),
-       ylim = c(0, 8),
-       font.lab = 2)
-  ## IPCC "likely" range
-  x <- c(1.0, 2.5, 2.5, 1.0)
-  y <- c(8, 8, 0, 0) 
-  polygon(x, y, col = "gray90", border = NA)
-  ## Noninformative
-  lines(density(tcr), lwd = 4, col = prior_cols[5])
-  ## Moderate lukewarmer prior
-  curve(dnorm(x, mean = 1, sd = 0.25), 
-        from = 0, to = 2, lwd = 4, lty = 2, col = prior_cols[4], add = T, yaxt = "n")
-  ## Strong lukewarmer prior
-  curve(dnorm(x, mean = 1, sd = 0.065), 
-        from = 0.5, to = 1.5, lwd = 4, lty = 2, col = prior_cols[3], add = T, yaxt = "n")
-  ## Moderate Denier prior
-  curve(dnorm(x, mean = 0, sd = 0.25), 
-        from = -1, to = 1, lwd = 4, lty = 2, col = prior_cols[2], add = T, yaxt = "n")
-  ## Strong denier prior
-  curve(dnorm(x, mean = 0, sd = 0.065),
-        from = -0.5, to = 0.5, lwd = 4, lty = 2, col = prior_cols[1], add = T, yaxt = "n")
-  ## Year tracker
-  if(recurse_type == "historic"){
-    text(1.75, 7, label = paste("Looking back to", yr_min), cex = 2)
-    # text(1.75,6.5, label = paste("(Sample size =", yr_max - yr_min + 1, "years)"), cex = 2)
-  }
-  if(recurse_type == "future"){
-    text(1.75, 7, label = paste("Looking forward to", max(clim_df$year)), cex = 2)
-    # text(1.75,6.5, label = paste("(Sample size =", nrow(clim_df), "years)"), cex = 2)
-  }
-  text(1.75,6.5, label = paste("(Sample size =", nrow(clim_df), "years)"), cex = 2)
-}
-## Moderate lukewarmer
-if(prior_type == "luke" & convic_type == "mod"){
-  lines(density(tcr), lwd = 4, col = prior_cols[4])
-}
-## Strong lukewarmer
-if(prior_type == "luke" & convic_type == "strong"){
-  lines(density(tcr), lwd = 4, col = prior_cols[3])
-}
-## Moderate Denier
-if(prior_type == "den" & convic_type == "mod"){
-  lines(density(tcr), lwd = 4, col = prior_cols[2])
-}
-## Strong denier
-if(prior_type == "den" & convic_type == "strong"){
-  lines(density(tcr), lwd = 4, col = prior_cols[1])
-  ## Legend
-  legend("topleft", 
-         prior_names, 
-         col = prior_cols,
-         cex = 2, lty = 1, lwd = 4, bty = "n")
-  dev.off()
-}
+tcr <- tcr %>% select_("-beta")
 
-tcr_rec[[a]] <- 
-  data.frame(year_to = ifelse(recurse_type == "historic", yr_min, max(clim_df$year)),
-             samp_size = nrow(clim_df),
-             series = paste0(prior_type, convic_type), 
-             mean = mean(tcr),
-             q025 = quantile(tcr, p = 0.025),
-             q975 = quantile(tcr, p = 0.975),
-             row.names = NULL
-             )
-
-rm(tcr, clim_df)
+return(tcr = tcr)
--- Recursive/sceptic-recursive.R
+++ Recursive/sceptic-recursive.R
@@ -7,11 +7,11 @@ source("sceptic_funcs.R")
 set.seed(123) 
 
 ## Load climate data
-climate <- read_csv("./Data/climate.csv")
+climate <- read_csv("Data/climate.csv")
 
 ## Decide on length of MCMC chains (including no. of chains in parallel JAGS model)
-chain_length <- 5000
-n_chains <- detectCores() - 1 
+chain_length <- 15000
+n_chains <- max(sapply(1:detectCores(), function(x) gcd(x, chain_length)))
 
 ## NB: Decide whether using historical data only (and work *backwards* from most recent 
 ## date), or whether using simulated future data (and work *fowards* from earliest data).
@@ -26,11 +26,12 @@ if (recurse_type == "future") {yr_max <- 2075}
 rcp_type <- "rcp26" 
 
 ## Load climate data and deviation DF for simulating "true" future values
-climate <- read_csv("./Data/climate.csv") %>%
+climate <- 
+  read_csv("Data/climate.csv") %>%
   filter(rcp == rcp_type) %>%
   filter(year <= yr_max) 
 
-y_dev <- read_csv("./Evidence/Data/y-dev.csv")
+y_dev <- read_csv("Evidence/Data/y-dev.csv")
 
 climate$noise <- sample(y_dev$dev, nrow(climate), replace = T)
 
@@ -57,88 +58,164 @@ climate <-
 ## Set radiative forcing distribution used for calulating TCRs later in code.
 ## Centered around 3.71 °C +/- 10% (within 95% CI). 
 ## Length of disbn equals length of MCMC chain for consistency
-rf2x <- rnorm(chain_length * n_chains, mean = 3.71, sd = 0.1855) 
+rf2x <- rnorm(chain_length, mean = 3.71, sd = 0.1855) 
 
-tcr_rec <- list()
-a <- 0 ## Count variable for combining TCR lists later
-A <- 0 ## Count variable for (combined TCR) animation figures
-
+## Priors data frame
+priors_df <- 
+  data_frame(mu = c(0, 1, 1, 0, 0),
+             sigma = c(100, 0.25, 0.065, 0.25, 0.065),
+             prior_type = c("ni", "luke", "luke", "den", "den"),
+             convic_type = c("", "mod", "strong", "mod", "strong")
+  )
+priors_df
 
 ## Loop over specified year intervals for recursive estimation
 if (recurse_type == "historic") {recurse_seq <- seq(from = yr_max - 15, to = 1866, by = -1)} 
 if (recurse_type == "future") {recurse_seq <- seq(from = 1880, to = yr_max, by = 1)}
 
-## Run the recursive regression for each prior type
+## Run the recursive nested regressions.
 ## NOTE: This takes about 25min to complete on my system. If you don't feel like
-## waiting that long (or even longer for older machines) then skip to line 105
+## waiting that long (or even longer for older machines) then skip to line 180
 ## and read in the saved results from a previous session.
-ptm <- proc.time()
-for (n in recurse_seq) {
-  ## Loop over prior ##
-  for (k in 1:3)  {  
-    prior_type <- c("ni", "luke", "den")[k] 
-    ## Loop over conviction strength ##
-    if(prior_type == "ni")  {
-      convic_type <- ""
-      # source("./Recursive/jags-recursive.R") ## For vague noninformative riors using the rjags package
-      source("./Recursive/noninf-recursive.R") ## For "proportional" noninformative prors using the LearnBayes package
+tcr_rec <-
+  ## Outer: Loop over recursive years
+  pblapply(seq_along(recurse_seq), function(n){
+    
+    if(recurse_type == "historic"){
+      clim_df <- 
+        climate %>%
+        filter(year >=  recurse_seq[n])
     }
-    else{for (j in 1:2)  {   
-      convic_type <- c("mod", "strong")[j]
-      source("./Recursive/jags-recursive.R")   
-    } } ## End of conviction loop
-  } ## End of prior loop
-} ## End of recursive loop
-proc.time() - ptm
-## Historic
-#   user   system  elapsed 
-# 42.004   28.320 1438.644
-## Same for Future set until 2005. But then, e.g., going then from 2006 to 2050...
-# user  system elapsed 
-# 18.55    5.07  752.94 
+    
+    if(recurse_type == "future"){
+      clim_df <- 
+        climate %>%
+        filter(year <=  recurse_seq[n]) 
+    }
+    
+    yr_min <- min(clim_df$year)
+    
+    ## Inner: Loop over priors ##
+    tcr_df <-
+      lapply(1:nrow(priors_df), function(j){
+        
+        m <- priors_df[j, ]$mu
+        s <- priors_df[j, ]$sigma
+        prior_type <- priors_df[j, ]$prior_type
+        convic_type <- priors_df[j, ]$convic_type 
+        
+        mu_beta <- m/3.71
+        sigma_beta <- s/3.71
+        
+        if(prior_type == "ni")  {
+          # source("Recursive/jags-recursive.R", local = T) ## For vague noninformative riors using the rjags package
+          source("Recursive/noninf-recursive-pblapply.R", local = T) ## For "proportional" noninformative prors using the LearnBayes package
+        }
+        else{
+          source("Recursive/jags-recursive-pblapply.R", local = T)
+        }
 
-tcr_rec <- dplyr::bind_rows(tcr_rec)
+      })
 
-write_csv(tcr_rec, paste0("./Recursive/Data/tcr-rec-", recurse_type, ".csv"))
+    ## Extract only the .$value elements (drop surplus .$visibility element)
+    tcr_df <-
+      lapply(1:length(tcr_df), function(x) tcr_df[[x]]$value) %>%
+      bind_rows()
+
+    ## Year tracker annotation label
+    year_tracker_lab <-
+      ifelse(
+        recurse_type == "historic",
+        paste0("Looking back to ", tcr_df$year_to[1],"\n(Sample size = ", 2005-tcr_df$year_to[1]+1, " years)"),
+        paste0("Looking forward to ", tcr_df$year_to[1],"\n(Sample size = ", tcr_df$year_to[1]-2005+1, " years)")
+        )
+
+    ## TCR Density plot
+    tcr_df %>%
+      mutate(prior = factor(match_priors(prior),
+                            levels = prior_names)) %>%
+      ggplot(aes(x = tcr, col = prior)) +
+      geom_line(stat = "density") +
+      labs(x = expression(~degree*C), y = "Density") +
+      xlim(-1, 3) +
+      annotate("rect", xmin = 1, xmax = 2.5, ymin = 0, ymax = Inf,
+               alpha = .2) +
+      stat_function(fun = dnorm, args = list(mean = 0, sd = .065),
+                    lty=2, col=prior_cols[1]) +
+      stat_function(fun = dnorm, args = list(mean = 0, sd = .25),
+                    lty=2, col=prior_cols[2]) +
+      stat_function(fun = dnorm, args = list(mean = 1, sd = .065),
+                    lty=2, col=prior_cols[3]) +
+      stat_function(fun = dnorm, args = list(mean = 1, sd = .25),
+                    lty=2, col=prior_cols[4]) +
+      annotate("text", x = 1.75, y = 5.75, label = year_tracker_lab, size = 3.5) +
+      scale_colour_manual(values = prior_cols) +
+      guides(col = guide_legend(nrow = 2)) +
+      theme_tcr +
+      ggsave(file = paste0("Recursive/TablesFigures/Animation/", recurse_type,
+                           "/rec-tcr-", suff, 1000 + n, ".pdf"),
+             width = 6, height = 4,
+             device = cairo_pdf)
+
+    tcr_rec <-
+      tcr_df %>% 
+      group_by(prior, year_to) %>%
+      summarise(
+        mean = mean(tcr),
+        q025 = quantile(tcr, p = 0.025),
+        q975 = quantile(tcr, p = 0.975)
+      )
+    
+    return(tcr_rec)
+  
+  }) %>%
+  bind_rows()
+  
+tcr_rec
+
+## Write to disk for future use
+write_csv(tcr_rec, paste0("Recursive/Data/tcr-rec-", recurse_type, ".csv"))
 
 
 ###################################
 ## Read data
-tcr_rec <- read_csv(paste0("./Recursive/Data/tcr-rec-", recurse_type,".csv"))
+tcr_rec <- read_csv(paste0("Recursive/Data/tcr-rec-", recurse_type,".csv"))
 
 tcr_rec <- 
   tcr_rec %>%
-  arrange(series) %>% 
-  mutate(serieslab = series)
+  arrange(prior) %>% 
+  mutate(priorlab = prior)
 
-## Next "rebind" the ni series data in the way that can be easily faceted by the four sceptic priors
-## Basically involves duplicating the ni series for each sceptic prior in the data frame
+## Next "rebind" the ni prior data in the way that can be easily faceted by the 
+## four sceptic priors... Basically involves duplicating the ni series for each
+## sceptic prior in the data frame
 tcr_rec <-
-  bind_rows(tcr_rec %>% filter(series != "ni"),
-            do.call("bind_rows",
-                    lapply(c("lukemod", "lukestrong", "denmod", "denstrong"),
-                           function(x) {
-                             tcr_rec %>%
-                               filter(series == "ni") %>%
-                               mutate(serieslab = x)
-                             }
-                           )))
+  bind_rows(
+    tcr_rec %>% 
+      filter(prior != "ni"),
+    lapply(c("lukemod", "lukestrong", "denmod", "denstrong"), function(x) {
+      tcr_rec %>%
+        filter(prior == "ni") %>%
+        mutate(priorlab = x)
+      }) %>%
+      bind_rows()
+    )
 
-tcr_rec$serieslab <- 
-  ifelse(tcr_rec$serieslab == "lukemod",
-         "(A) Moderate Lukewarmer", 
-         ifelse(tcr_rec$serieslab == "lukestrong",
-                "(B) Strong Lukewarmer",
-                ifelse(tcr_rec$serieslab == "denmod",
-                       "(C) Moderate Denier",
-                       "(D) Strong Denier"
+tcr_rec$priorlab <- 
+  ifelse(tcr_rec$priorlab == "lukemod",
+         "(a) Moderate Lukewarmer", 
+         ifelse(tcr_rec$priorlab == "lukestrong",
+                "(b) Strong Lukewarmer",
+                ifelse(tcr_rec$priorlab == "denmod",
+                       "(c) Moderate Denier",
+                       "(d) Strong Denier"
                        )))
 
 tcr_plot <- 
-  ggplot(tcr_rec, aes(x = year_to, #x = samp_size,
-                     y = mean, col = series)) +
+  tcr_rec %>%
+  ggplot(aes(x = year_to, y = mean, col = prior)) +
   geom_line(lwd = .85) +
-  geom_ribbon(aes(ymin = q025, ymax = q975, fill = series), lty = 0, alpha = 0.4) +
+  geom_ribbon(aes(ymin = q025, ymax = q975, fill = prior), lty = 0, alpha = 0.4) +
   ylab(expression(~degree~C)) + xlab("Year") + #xlab("Sample size (years)") +
   ## scale_y_continuous(limits = c(-1, 3)) +
   scale_colour_manual(values = prior_cols, 
@@ -148,7 +225,7 @@ tcr_plot <-
                     limits = c("denstrong","denmod","lukestrong","lukemod","ni"),
                     labels = prior_names) +
   background_grid(major = "y", minor = "none", colour.major = "gray90") +
-  facet_wrap(~ serieslab, ncol = 2) +
+  facet_wrap(~ priorlab, ncol = 2) +
   theme_recursive 
 
 if(recurse_type == "historic"){
@@ -159,37 +236,6 @@ if(recurse_type == "historic"){
   }
 
 tcr_plot +
-  ggsave(file = paste0("./Recursive/TablesFigures/rec-tcr-all-", recurse_type, ".pdf"),
-         width = 8, height = 7, 
-         device = cairo_pdf)
-
-
-## As above, but using stippled lines for 95% CI instead of geom_ribbon
-tcr_plot_lines <- 
-  ggplot(tcr_rec, aes(x = year_to, col = series)) +
-  geom_line(aes(y = mean), lwd = .85) +
-  geom_line(aes(y = q025), lty = 2) +
-  geom_line(aes(y = q975), lty = 2) +
-  ylab(expression(~degree~C)) + xlab("Year") + #xlab("Sample size (years)") +
-  ## scale_y_continuous(limits = c(-1, 3)) +
-  scale_colour_manual(values = prior_cols, 
-                      limits = c("denstrong","denmod","lukestrong","lukemod","ni"),
-                      labels = prior_names) +
-  scale_fill_manual(values = c(prior_cols[1:4], "gray60"), 
-                    limits = c("denstrong","denmod","lukestrong","lukemod","ni"),
-                    labels = prior_names) +
-  background_grid(major = "y", minor = "none", colour.major = "gray90") +
-  facet_wrap(~ serieslab, ncol = 2) +
-  theme_recursive 
-
-if(recurse_type == "historic"){
-  tcr_plot_lines <- 
-    tcr_plot_lines + 
-    scale_x_reverse(breaks = seq(max(tcr_rec$year_to), min(tcr_rec$year_to), by = -30)) +
-    xlab("Year")
-}
-
-tcr_plot_lines +
-  ggsave(file = paste0("./Recursive/TablesFigures/rec-tcr-all-", recurse_type, "-lines.pdf"),
+  ggsave(file = paste0("Recursive/TablesFigures/rec-tcr-all-", recurse_type, suff, ".pdf"),
          width = 8, height = 7, 
          device = cairo_pdf)
